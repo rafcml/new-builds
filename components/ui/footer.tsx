04a4e4495f6c52b4d2466eccb4b28aa301750105
raf <rafvitale@chainml.net>
2024-10-03 10:13:48 -0400

    footer and text

---
+++ components/ui/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ textAlign: "center", padding: "20px", marginTop: "auto" }}>
+      Photos taken by Raf and Omar
+    </footer>
+  );
+};
+
+export default Footer;
